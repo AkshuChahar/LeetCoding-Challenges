877e83517fc269373528b04ff7592cc6dd3721c8
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-08-02 22:28:38 +0530

    Add files via upload

---
+++ 08 - August 2022/Day 02 - Kth Smallest Element in a Sorted Matrix/Solution.py
@@ -0,0 +1,19 @@
+﻿class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        low, high, n = matrix[0][0], matrix[-1][-1], len(matrix)
+        # Helper function to get the count of numbers smaller than a given number.
+        def helper(num):
+            count = 0
+            # Traverse the matrix.
+            for i in range(n):
+                # Increment the count of smaller numbers.
+                count += bisect_right(matrix[i], num)
+            return count
+        # Perform 'Binary Search'.
+        while low < high:
+            mid = (low + high)//2
+            if helper(mid) < k:
+                low = mid + 1
+            else:
+                high = mid
+        return low
