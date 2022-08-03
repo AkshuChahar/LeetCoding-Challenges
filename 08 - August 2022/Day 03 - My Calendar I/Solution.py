6b352b3b067cbe09f348ce0238e1aeaeb05440fc
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-08-03 23:10:49 +0530

    Add files via upload

---
+++ 08 - August 2022/Day 03 - My Calendar I/Solution.py
@@ -0,0 +1,20 @@
+﻿class MyCalendar:
+
+    def __init__(self):
+        # Initialise the calender as a list of tupples.
+        self.calender = []
+
+    def book(self, start: int, end: int) -> bool:
+        # Traverse the calender.
+        for s, e in self.calender:
+            # Check if the current event satisfies the conditions.
+            if s < end and start < e:
+                return False
+        # Add the time to the list.
+        self.calender.append((start, end))
+        return True
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
