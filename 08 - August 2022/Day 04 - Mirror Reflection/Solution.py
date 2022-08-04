776d7a9960c869cb560ac29259a8025b4c8cc7b0
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-08-04 22:53:50 +0530

    Add files via upload

---
+++ 08 - August 2022/Day 04 - Mirror Reflection/Solution.py
@@ -0,0 +1,14 @@
+﻿class Solution:
+    def mirrorReflection(self, p: int, q: int) -> int:
+        # Divide by 2 while both 'p' and 'q' are even.
+        while (p % 2 == 0 and q % 2 == 0):
+            p /= 2
+            q /= 2
+        # Check if 'p' is even.
+        if p % 2 == 0:
+            return 2
+        # Check if 'q' is even.
+        elif q % 2 == 0:
+            return 0
+        else:
+            return 1
