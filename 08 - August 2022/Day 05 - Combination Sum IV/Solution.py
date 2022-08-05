536c8ccf76c96d9d62f2982a109fd1f80f9f3f57
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-08-05 21:17:39 +0530

    Add files via upload

---
+++ 08 - August 2022/Day 05 - Combination Sum IV/Solution.py
@@ -0,0 +1,21 @@
+﻿class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        # Initialise a 'dp' dictionary to keep track of various combination sums possible.
+        dp = {}
+        dp[0] = [1]
+        # Funtion to recursively search if the 'target - sum' combination exists.
+        def helper(target):
+            # Check if the current combination sum already exists in the dictionary.
+            if target in dp:
+                return dp[target]
+            summ = 0
+            # Traverse the array and check for each combination.
+            for num in nums:
+                if target > num:
+                    summ += helper(target - num)
+                elif target == num:
+                    summ += 1
+            # Update the dictionary.
+            dp[target] = summ
+            return summ
+        return helper(target)
