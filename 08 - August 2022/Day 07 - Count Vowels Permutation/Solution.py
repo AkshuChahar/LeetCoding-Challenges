2b215296ef6acea8c2d787cfe27dc9539a176cbb
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-08-07 12:27:23 +0530

    Add files via upload

---
+++ 08 - August 2022/Day 07 - Count Vowels Permutation/Solution.py
@@ -0,0 +1,9 @@
+﻿class Solution:
+    def countVowelPermutation(self, n: int) -> int:
+        # Initialise the count of vowels to 1.
+        a, e, i, o, u = 1, 1, 1, 1, 1
+        # Iterate from 2 to 'n'.
+        for _ in range(2, n + 1):
+            # Add the counts.
+            a, e, i, o, u = e + i + u, a + i, e + o, i, i + o
+        return (a + e+ i + o + u) % 1000000007
