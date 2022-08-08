942f3d449ec5ad47df42fa9d28edcf41cbe4e376
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-08-08 21:31:45 +0530

    Add files via upload

---
+++ 08 - August 2022/Day 08 - Longest Increasing Subsequence/Solution.py
@@ -0,0 +1,13 @@
+﻿class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # Initialise an array with the first element as 'nums[0]'.
+        arr = [nums.pop(0)]
+        # Traverse through the array.
+        for num in nums:
+            # If the current element is greater than the last element of 'arr', append it to 'arr'.
+            if (num > arr[-1]):
+                arr.append(num)
+            # Replace an element such that 'arr' remains increasing.
+            else:
+                arr[bisect_left(arr, num)] = num
+        return len(arr)
