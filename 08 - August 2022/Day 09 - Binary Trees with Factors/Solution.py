fc2056f0eb6a9df6568d2bc8ef6e402d4494a6ce
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-08-09 18:23:58 +0530

    Add files via upload

---
+++ 08 - August 2022/Day 09 - Binary Trees with Factors/Solution.py
@@ -0,0 +1,20 @@
+﻿class Solution:
+    def numFactoredBinaryTrees(self, arr: List[int]) -> int:
+        MOD, n = 10 ** 9 + 7, len(arr)
+        # Sort the array.
+        arr.sort()
+        # Initialise a 'dp' array.
+        dp = [1] * n
+        # Build a dictionary to get the index of a number quickly.
+        index = {x : i for i, x in enumerate(arr)}
+        # Traverse the array.
+        for i, x in enumerate(arr):
+            for j in range(i):
+                # Check if the given number can be a divisor of 'arr[i]'.
+                if x % arr[j] == 0:
+                    k = x / arr[j]
+                    # Get the index of the 'k'.
+                    if k in index:
+                        dp[i] += dp[j] * dp[index[k]]
+                        dp[i] %= MOD
+        return sum(dp) % MOD
