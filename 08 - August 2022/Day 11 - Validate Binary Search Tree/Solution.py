2440a84f3c9d4da0fa7974f324065e27611cd16f
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-08-11 22:44:01 +0530

    Add files via upload

---
+++ 08 - August 2022/Day 11 - Validate Binary Search Tree/Solution.py
@@ -0,0 +1,20 @@
+﻿# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        # Define a function to check if the current node value falls into valid range.
+        def check(node, min_val, max_val):
+            # If we have reached the end of subtree, return 'True'.
+            if node is None:
+                return True
+            # Check for validation.
+            if node.val <= min_val or node.val >= max_val:
+                return False
+            # Check for next node.
+            return check(node.left, min_val, node.val) and check(node.right, node.val, max_val)
+        return check(root, -2**31 - 1, 2**31)
