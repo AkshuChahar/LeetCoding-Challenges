221ef9ec39594ac9b71ed074f8bc6287d5ecda70
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-08-16 20:17:53 +0530

    Add files via upload

---
+++ 08 - August 2022/Day 16 - First Unique Character in a String/Solution.py
@@ -0,0 +1,9 @@
+﻿class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        # Initialise a dictionary and store the frequency of each character.
+        d = collections.Counter(s)
+        # Traverse the string and return the first character index with frequency 1.
+        for index, ch in enumerate(s):
+            if d[ch] == 1:
+                return index
+        return -1
