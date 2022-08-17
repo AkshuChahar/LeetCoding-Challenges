f1bf19b353abc38ad906e4bbb3cb6afde73f8ded
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-08-17 20:24:22 +0530

    Add files via upload

---
+++ 08 - August 2022/Day 17 - Unique Morse Code Words/Solution.py
@@ -0,0 +1,7 @@
+﻿class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        # Initialise an array with the Morse codes.
+        morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        # Initialise a set to store all the unique morse words.
+        ans = {"".join(morse[ord(ch) - ord('a')] for ch in word) for word in words}
+        return len(ans)
