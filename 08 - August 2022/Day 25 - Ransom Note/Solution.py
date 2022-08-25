25ba7c9f2d4550c7fc4e2037da395636a4b88e66
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-08-25 22:40:26 +0530

    Add files via upload

---
+++ 08 - August 2022/Day 25 - Ransom Note/Solution.py
@@ -0,0 +1,10 @@
+﻿class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        # Initialise two dictionaries and store the frequencies of each character in the string.
+        rn, mz = Counter(ransomNote), Counter(magazine)
+        # Traverse the dictionary 'rn'.
+        for ch in rn:
+            # Check if we have the required number of characters in 'magazine'.
+            if rn[ch] > mz[ch]:
+                return False
+        return True
