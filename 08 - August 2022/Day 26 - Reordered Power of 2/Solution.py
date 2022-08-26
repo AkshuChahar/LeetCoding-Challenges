3376eb381794933fe052b5b55b4c694a546c4e50
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-08-27 01:16:17 +0530

    Add files via upload

---
+++ 08 - August 2022/Day 26 - Reordered Power of 2/Solution.py
@@ -0,0 +1,10 @@
+﻿class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        # Get the frequency of each digit in the given number.
+        freq = Counter(str(n))
+        # Compare with all powers of 2.
+        for i in range(30):
+            # Check if the given number has same frequency of digits as any power of 2.
+            if freq == Counter(str(1 << i)):
+                return True
+        return False
