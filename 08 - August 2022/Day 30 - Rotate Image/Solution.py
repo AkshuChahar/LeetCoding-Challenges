e917cb047b2c49792da06dadd46b2d22de29d622
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-08-31 02:28:40 +0530

    Add files via upload

---
+++ 08 - August 2022/Day 30 - Rotate Image/Solution.py
@@ -0,0 +1,23 @@
+﻿class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        # First transpose the matrix.
+        self.transpose(matrix)
+        # Reverse the rows to get the result.
+        self.reverse(matrix)
+
+    # Function to transpose the matrix around the main diagonal.
+    def transpose(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+
+    # Function to reverse the rows of the matrix.
+    def reverse(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
