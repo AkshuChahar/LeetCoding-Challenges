c75cf80826dfdda515129fd28beb8cc388f8b61a
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-09-08 22:49:51 +0530

    Add files via upload

---
+++ 09 - September 2022/Day 08 - Binary Tree Inorder Traversal/Solution.py
@@ -0,0 +1,28 @@
+﻿# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        # Check if the 'root' node is NULL.
+        if not root:
+            return ans
+        # Initialise a atack for traversal.
+        s = deque()
+        curr = root
+        # Traverse the tree.
+        while s or curr:
+            # If the current node is not NULL, keep moving left.
+            if curr:
+                s.append(curr)
+                curr = curr.left
+            # If the current node is NULL, move towards right and update 'ans'.
+            else:
+                node = s.pop()
+                ans.append(node.val)
+                curr = node.right
+        return ans
