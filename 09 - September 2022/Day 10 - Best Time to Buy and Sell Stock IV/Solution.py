7752ed28485a9db043f629607242736dddff7936
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-09-10 18:39:29 +0530

    Add files via upload

---
+++ 09 - September 2022/Day 10 - Best Time to Buy and Sell Stock IV/Solution.py
@@ -0,0 +1,12 @@
+﻿class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        n = len(prices)
+        # Initialise arrays to store the minimum prices and maximum profits.
+        buy, profit = [float(inf)] * (k + 1), [0] * (k + 1)
+        # Traverse the array.
+        for price in prices:
+            # Update the arrays.
+            for i in range(1, k + 1):
+                buy[i] = min(buy[i], price - profit[i - 1])
+                profit[i] = max(profit[i], price - buy[i])
+        return profit[k]
