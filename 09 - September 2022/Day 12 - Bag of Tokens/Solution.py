29cbc558c645c29b67511485d76378c4ca11b9c1
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-09-12 23:25:34 +0530

    Add files via upload

---
+++ 09 - September 2022/Day 12 - Bag of Tokens/Solution.py
@@ -0,0 +1,22 @@
+﻿class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        ans, res, n = 0, 0, len(tokens)
+        # Sort the 'tokens'.
+        tokens.sort()
+        # Initialise two pointers for traversal.
+        left, right = 0, n - 1
+        # Traverse the array.
+        while (left <= right) and (power >= tokens[left] or res):
+            # Check if we can play the token 'face-up'.
+            while (left <= right) and (power >= tokens[left]):
+                power -= tokens[left]
+                left += 1
+                res += 1
+            # Update 'ans'.
+            ans = max(ans, res)
+            # Check if we can play the token 'face-down'.
+            if (left <= right) and res:
+                power += tokens[right]
+                right -= 1
+                res -= 1
+        return ans
