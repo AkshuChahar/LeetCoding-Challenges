5933e096137f495fac4819a94564d7321cc7ec6d
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-09-14 22:33:15 +0530

    Add files via upload

---
+++ 09 - September 2022/Day 13 - UTF-8 Validation/Solution.py
@@ -0,0 +1,25 @@
+﻿class Solution:
+    def validUtf8(self, data: List[int]) -> bool:
+        byte = 0
+        # Traverse the array.
+        for num in data:
+            # Convert the number into binary and get the 8 least significant digits.
+            binary = format(num, '#010b')[-8:]
+            # Get the number of bytes.
+            if byte == 0:
+                for bit in binary:
+                    if bit == '0':
+                        break
+                    byte += 1
+                # Check if we a 1-byte character.
+                if byte == 0:
+                    continue
+                # Check if the bytes are between 1 and 4.
+                if byte == 1 or byte > 4:
+                    return False
+            else:
+                # Check if the subsequent numbers adhere to the rules.
+                if not (binary[0] == '1' and binary[1] == '0'):
+                    return False
+            byte -= 1
+        return byte == 0
---
+++ 09 - September 2022/Day 14 - Pseudo-Palindromic Paths in a Binary Tree/Solution.py
@@ -0,0 +1,26 @@
+﻿# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+        ans = 0
+        # Initialise a stack for 'Preorder Traversal' and to keep track of frequency of odd count of numbers.
+        st = [(root, 0)]
+        while st:
+            node, count = st.pop()
+            if node:
+                # Compute the frequency.
+                count ^= (1 << node.val)
+                # Check if we have arrived at a leaf node.
+                if (not node.left) and (not node.right):
+                    # Check if we have atmpst 1 number with odd frequency.
+                    if count & (count - 1) == 0:
+                        ans += 1
+                else:
+                    st.append((node.right, count))
+                    st.append((node.left, count))
+        return ans
