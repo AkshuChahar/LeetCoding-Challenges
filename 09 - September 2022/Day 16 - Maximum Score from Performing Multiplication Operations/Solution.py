1d06d66c434e4677f1829d5a1a19a32c411da4f0
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-09-16 11:08:33 +0530

    Add files via upload

---
+++ 09 - September 2022/Day 16 - Maximum Score from Performing Multiplication Operations/Solution.py
@@ -0,0 +1,12 @@
+﻿class Solution:
+    def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
+        m, n = len(multipliers), len(nums)
+        # Initialise the 'dp' array.
+        dp = [0] * (m + 1)
+        for i in range(m - 1, -1, -1):
+            # Copy the present 'dp' array to serve as the next row.
+            row = dp.copy()
+            # Iterate to fill the current row.
+            for j in range(i, -1, -1):
+                dp[j] = max(multipliers[i] * nums[j] + row[j + 1], multipliers[i] * nums[n - 1 - i + j] + row[j])
+        return dp[0]
