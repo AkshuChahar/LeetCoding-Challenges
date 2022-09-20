e9cec4676611ce21a6221b97d9eec979479ceb29
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-09-20 23:32:40 +0530

    Add files via upload

---
+++ 09 - September 2022/Day 20 - Maximum Length of Repeated Subarray/Solution.py
@@ -0,0 +1,17 @@
+﻿class Solution:
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        ans = 0
+        # Join the second list to get a string.
+        str2 = "".join([chr(num) for num in nums2])
+        str1 = ""
+        # Traverse the first list.
+        for num in nums1:
+            # Append to the 'str1'.
+            str1 += chr(num)
+            # Check if the current string is present in 'str2'.
+            if str1 in str2:
+                ans = max(ans, len(str1))
+            # Check by slicing the string.
+            else:
+                str1 = str1[1:]
+        return ans
