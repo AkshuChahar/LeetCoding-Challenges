0f749dc7a0f49125b1b51936718ef545f6e08838
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-09-22 22:38:12 +0530

    Add files via upload

---
+++ 09 - September 2022/Day 22 - Reverse Words in a String III/Solution.py
@@ -0,0 +1,6 @@
+﻿class Solution:
+    def reverseWords(self, s: str) -> str:
+        # Split the string around whitespaces.
+        l = s.split(" ")
+        # Reverse and join the strings.
+        return " ".join([s[::-1] for s in l])
