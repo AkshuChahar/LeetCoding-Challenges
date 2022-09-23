19aeddcc295b897f1e78f0929e8c6c52f6134a34
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-09-23 22:55:29 +0530

    Add files via upload

---
+++ 09 - September 2022/Day 23 - Concatenation of Consecutive Binary Numbers/Solution.py
@@ -0,0 +1,4 @@
+﻿class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+        # Loop over 1 to n and concatenate the binary representations.
+        return int("".join([str(bin(i))[2:] for i in range(1, n + 1)]), 2) % (10 ** 9 + 7)
