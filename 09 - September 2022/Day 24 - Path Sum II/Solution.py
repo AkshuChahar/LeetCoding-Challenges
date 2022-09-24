e022669e47ce3ef7589778711019a13aa0e9ec47
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-09-24 17:20:07 +0530

    Add files via upload

---
+++ 09 - September 2022/Day 24 - Path Sum II/Solution.py
@@ -0,0 +1,28 @@
+﻿# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:    
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        ans = []
+        # Function for 'Backtracking'.
+        def backtrack(root, targetSum, ans, res):
+            # Check if we have reached the leaf node.
+            if not root:
+                return
+            # Add the current node to the 'res' list.
+            res.append(root.val)
+            # Check if we have reached the 'targetSum'.
+            if (not root.left and not root.right and targetSum == root.val):
+                ans.append(list(res))
+            # Move to the left and right subtree.
+            backtrack(root.left, targetSum - root.val, ans, res)
+            backtrack(root.right, targetSum - root.val, ans, res)
+            # Backtrack.
+            res.pop()
+        # Call the recursive function.
+        backtrack(root, targetSum, ans, [])
+        return ans
