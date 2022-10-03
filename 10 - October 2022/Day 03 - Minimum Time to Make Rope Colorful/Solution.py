d08a4921e04aab51030c0566ffdc794d356ad0a7
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-10-03 23:52:18 +0530

    Add files via upload

---
+++ 10 - October 2022/Day 03 - Minimum Time to Make Rope Colorful/Solution.py
@@ -0,0 +1,14 @@
+﻿class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        n, ans = len(colors), 0
+        # Initialise variable to store the maximum time in a group.
+        time = neededTime[0]
+        # Traverse the 'colors'.
+        for i in range(1, n):
+            # Check if the color of the current balloon is different from the previous one.
+            if colors[i] != colors[i - 1]:
+                time = 0
+            # Update 'ans' and 'time'.
+            ans += min(time, neededTime[i])
+            time = max(time, neededTime[i])
+        return ans
