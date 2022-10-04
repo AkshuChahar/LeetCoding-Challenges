f79baa6e98b7ab76479ac9d30558c0c0f78e0d48
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-10-05 01:07:28 +0530

    Add files via upload

---
+++ 10 - October 2022/Day 04 - Path Sum/Solution.py
@@ -0,0 +1,21 @@
+﻿# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        # Edge case: Check if we have reached the end of the path.
+        if not root:
+            return False
+        # Check if we have reached the leaf node and the acquired the sum.
+        if (not root.left) and (not root.right):
+            if targetSum == root.val:
+                return True
+            return False
+        # Check for next node.
+        if self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum - root.val):
+            return True
+        return False
