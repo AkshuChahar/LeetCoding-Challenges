e6d93ccc8371c373438437a051dc1411e5c2266a
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-10-09 21:34:25 +0530

    Add files via upload

---
+++ 10 - October 2022/Day 09 - Two Sum IV - Input is a BST/Solution.py
@@ -0,0 +1,27 @@
+﻿# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        # Initialise a dictionary to store the elements and queue for traversal of the tree.
+        d = defaultdict()
+        q = deque()
+        q.append(root)
+        while len(q):
+            n = len(q)
+            while n:
+                node = q.popleft()
+                # Check if we got the required sum.
+                if d.get(k - node.val):
+                    return True
+                d[node.val] = 1
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                n -= 1
+        return False
