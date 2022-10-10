b10483b74e77750a50c54fe1683e7d188ec74dc9
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-10-10 23:18:40 +0530

    Add files via upload

---
+++ Day 10 - Break a Palindrome/Solution.py
@@ -0,0 +1,11 @@
+﻿class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        n = len(palindrome)
+        # Edge case: Check if the length of string is 1.
+        if n == 1:
+            return ""
+        # Iterate over the string and change the first non-'a' character to 'a'.
+        for i in range(n//2):
+            if palindrome[i] != 'a':
+                return palindrome[:i] + 'a' + palindrome[i + 1:]
+        return palindrome[:-1] + 'b'
