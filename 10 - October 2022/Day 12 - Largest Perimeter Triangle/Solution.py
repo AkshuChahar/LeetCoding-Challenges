9199f3ea3796e67d7442ccf5908b7fb3a70a384b
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-10-12 21:56:19 +0530

    Add files via upload

---
+++ 10 - October 2022/Day 12 - Largest Perimeter Triangle/Solution.py
@@ -0,0 +1,10 @@
+﻿class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        n = len(nums)
+        # Sort the lengths.
+        nums.sort()
+        # Check if the triangle is possible to be formed.
+        for i in range(n - 1, 1, -1):
+            if nums[i] < nums[i - 1] + nums[i - 2]:
+                return nums[i] + nums[i - 1] + nums[i - 2]
+        return 0
