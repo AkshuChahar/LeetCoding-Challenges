c67e7c35d879baeb484a87becf04312681e87f62
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-10-13 18:29:51 +0530

    Add files via upload

---
+++ 10 - October 2022/Day 13 - Delete Node in a Linked List/Solution.py
@@ -0,0 +1,19 @@
+﻿# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        # Change the value of the current node.
+        node.val = node.next.val
+        # Delete the next node.
+        temp = node.next
+        node.next = node.next.next
+        temp.next = None
+        del(temp)
