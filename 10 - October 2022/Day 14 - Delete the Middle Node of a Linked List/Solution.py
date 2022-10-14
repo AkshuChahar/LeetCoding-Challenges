fe4add51422ac9b77acdbf76f7f4ea2040539569
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-10-14 20:53:54 +0530

    Add files via upload

---
+++ 10 - October 2022/Day 14 - Delete the Middle Node of a Linked List/Solution.py
@@ -0,0 +1,26 @@
+﻿# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Edge case: Check if only one exists.
+        if not head.next:
+            return None
+        # Initialise two pointers.
+        slow, fast = head, head.next.next
+        # Iterate till end.
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        # Check if the linked list has even length.
+        if fast and fast.next:
+            slow = slow.next
+        # Delete the node.
+        temp = slow.next
+        slow.next = temp.next
+        temp.next = None
+        del(temp)
+        return head
