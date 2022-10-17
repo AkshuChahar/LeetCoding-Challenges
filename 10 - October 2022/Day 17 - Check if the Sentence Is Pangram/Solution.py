cc08b26a582d4384fdda9d051fdc1a93bd39c672
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-10-17 22:01:21 +0530

    Add files via upload

---
+++ 10 - October 2022/Day 17 - Check if the Sentence Is Pangram/Solution.py
@@ -0,0 +1,5 @@
+﻿class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        # Add all the letters to a 'set' and check if the length of set is '26'.
+        s = set(sentence)
+        return len(s) == 26
