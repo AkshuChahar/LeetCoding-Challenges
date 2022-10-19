711b5db1ddb7fb0b38215ec4deff5b87eb87a1ec
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-10-19 23:26:08 +0530

    Add files via upload

---
+++ 10 - October 2022/Day 19 - Top K Frequent Words/Solution.py
@@ -0,0 +1,9 @@
+﻿class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        # Initialise a dictionary and store the frequencies of the words.
+        d = defaultdict(int)
+        for word in words:
+            d[word] += 1
+        # Sort the dictionary.
+        ans = sorted(d, key = lambda x: (-d[x], x))
+        return ans[:k]
