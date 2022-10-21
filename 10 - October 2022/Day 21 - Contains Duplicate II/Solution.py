469517a490074e0c10edb788e2e1c5193b91c056
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-10-21 19:00:25 +0530

    Add files via upload

---
+++ 10 - October 2022/Day 21 - Contains Duplicate II/Solution.py
@@ -0,0 +1,11 @@
+﻿class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # Initialise a dictionary to store the visited elements.
+        d = {}
+        # Traverse the list.
+        for i, num in enumerate(nums):
+            # Check if the two elements are equal and the distance between them is <= 'k'.
+            if num in d and i - d[num] <= k:
+                return True
+            d[num] = i
+        return False
