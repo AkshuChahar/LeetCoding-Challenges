95c0b575b3387c9e7a1bc5e7014cb31314e555e4
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-10-23 23:24:48 +0530

    Add files via upload

---
+++ 10 - October 2022/Day 23 - Set Mismatch/Solution.py
@@ -0,0 +1,8 @@
+﻿class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        # Get the 'desired' and 'actual' sum.
+        desired = (len(nums) * (len(nums) + 1)) // 2
+        actual = sum(nums)
+        # Get the sum of unique elements.
+        s = sum(set(nums))
+        return [actual - s, desired - s]
