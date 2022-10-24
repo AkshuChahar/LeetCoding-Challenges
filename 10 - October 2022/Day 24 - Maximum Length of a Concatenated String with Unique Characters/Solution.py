d2e4fce57cf46d72d91853dc362023e95fcab6e8
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-10-24 15:50:31 +0530

    Add files via upload

---
+++ 10 - October 2022/Day 24 - Maximum Length of a Concatenated String with Unique Characters/Solution.py
@@ -0,0 +1,16 @@
+﻿class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+        n, ans = len(arr), 0
+        # Initialise a list to store the various combinations.
+        res = [""]
+        # Traverse the array.
+        for word in arr:
+            for r in res:
+                comb = r + word
+                # Check if there is a duplicate element.
+                if len(comb) != len(set(comb)):
+                    continue
+                # Update 'res' and 'ans'.
+                res.append(comb)
+                ans = max(ans, len(comb))
+        return ans
