c2888f4c9d574af3c246d89adcb2d23a3ec8a3a9
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-10-28 19:05:44 +0530

    Add files via upload

---
+++ 10 - October 2022/Day 28 - Group Anagrams/Solution.py
@@ -0,0 +1,11 @@
+﻿class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Initialise a dictionary to store the visited words in their characters sorted.
+        d = defaultdict(list)
+        # Traverse the list.
+        for word in strs:
+            # Sort the characters of the string and update the dictionary.
+            sorted_word = ''.join(sorted(word))
+            d[sorted_word].append(word)
+        ans = [val for val in d.values()]
+        return ans
