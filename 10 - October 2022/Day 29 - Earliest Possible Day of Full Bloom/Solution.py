ee9dc583211a445aa01f7f64df9961f9b9654373
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-10-29 21:07:56 +0530

    Add files via upload

---
+++ 10 - October 2022/Day 29 - Earliest Possible Day of Full Bloom/Solution.py
@@ -0,0 +1,10 @@
+﻿class Solution:
+    def earliestFullBloom(self, plantTime: List[int], growTime: List[int]) -> int:
+        ans, res = 0, 0
+        # Sort the 'growTime' list and store the indices.
+        ind = sorted(range(len(plantTime)), key = lambda x: -growTime[x])
+        # Traverse the indices in sorted order.
+        for i in ind:
+            res += plantTime[i]
+            ans = max(ans, res + growTime[i])
+        return ans
