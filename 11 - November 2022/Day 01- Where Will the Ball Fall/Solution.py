7a9f47b21d4477c9a11d4089088fb47e4cc44c23
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-11-01 23:42:09 +0530

    Add files via upload

---
+++ 11 - November 2022/Day 01- Where Will the Ball Fall/Solution.py
@@ -0,0 +1,18 @@
+﻿class Solution:
+    def findBall(self, grid: List[List[int]]) -> List[int]:
+        m, n = len(grid), len(grid[0])
+        ans = []
+        # Function for 'DFS'.
+        def dfs(row, col):
+            # Check if the ball is going out of the box.
+            if row == m:
+                return col
+            new_col = col + grid[row][col]
+            # Check if the ball is getting stuck.
+            if new_col == n or new_col == -1 or grid[row][new_col] != grid[row][col]:
+                return -1
+            else:
+                return dfs(row + 1, new_col)
+        for col in range(n):
+            ans.append(dfs(0, col))
+        return ans
