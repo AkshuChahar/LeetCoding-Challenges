d4eab05d23c70fc0bd632f52dc65454d3e6889d1
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-11-02 22:12:52 +0530

    Add files via upload

---
+++ 11 - November 2022/Day 02 - Minimum Genetic Mutation/Solution.py
@@ -0,0 +1,19 @@
+﻿class Solution:
+    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
+        # Initialise a queue for 'BFS' and a set to keep track of visited strings.
+        q = deque([(start, 0)])
+        vis = {start}
+        # Do a 'BFS'.
+        while q:
+            s, ans = q.popleft()
+            # Check if we have arrived at the end.
+            if s == end:
+                return ans
+            # Check for each character.
+            for c in "ACGT":
+                for i in range(len(s)):
+                    temp = s[:i] + c + s[i + 1:]
+                    if temp not in vis and temp in bank:
+                        q.append((temp, ans + 1))
+                        vis.add(temp)
+        return -1
