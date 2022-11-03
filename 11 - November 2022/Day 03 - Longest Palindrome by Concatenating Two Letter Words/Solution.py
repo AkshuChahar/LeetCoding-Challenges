c0537b2dbbe21ad0d56d439833afb19556f64657
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-11-04 01:53:05 +0530

    Add files via upload

---
+++ 11 - November 2022/Day 03 - Longest Palindrome by Concatenating Two Letter Words/Solution.py
@@ -0,0 +1,21 @@
+﻿class Solution:
+    def longestPalindrome(self, words: List[str]) -> int:
+        ans = 0
+        # Initialise a dictionary to count the frequency of each word and a flag to check if we have a central word.
+        freq = Counter(words)
+        center = False
+        # Traverse the dictionary.
+        for word, count in freq.items():
+            # Check if the word is a palindrome.
+            if word[0] == word[1]:
+                # Update 'ans' and the flag.
+                if count % 2 == 0:
+                    ans += count
+                else:
+                    ans += count - 1
+                    center = True
+            elif word[0] < word[1]:
+                ans += 2 * min(count, freq[word[1] + word[0]])
+        if center:
+            ans += 1
+        return 2 * ans
