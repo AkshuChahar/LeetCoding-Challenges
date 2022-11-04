f27b4c28b89391a7b389e56e2e94fd9a3c765b22
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-11-04 23:13:36 +0530

    Add files via upload

---
+++ 11 - November 2022/Day 04 - Reverse Vowels of a String/Solution.py
@@ -0,0 +1,21 @@
+﻿class Solution:
+    def reverseVowels(self, st: str) -> str:
+        s = [c for c in st]
+        n = len(s)
+        # Initialise two pointers.
+        left, right = 0, n - 1
+        # Traverse the string.
+        while left < right:
+            # Move to the next vowels.
+            while left <= right and s[left] not in "aeiouAEIOU":
+                left += 1
+            while right >= left and s[right] not in "aeiouAEIOU":
+                right -= 1
+            # Swap the vowels.
+            if left < right:
+                temp = s[left]
+                s[left] = s[right]
+                s[right] = temp
+            left += 1
+            right -= 1
+        return "".join(s)
