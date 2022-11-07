626faf3fe322350451883260dfebf21add3ef8fe
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-11-07 23:06:07 +0530

    Add files via upload

---
+++ 11 - November 2022/Day 07 - Maximum 69 Number/Solution.py
@@ -0,0 +1,5 @@
+﻿class Solution:
+    def maximum69Number (self, num: int) -> int:
+        # Convert the number to a string and change the first '6' to '9'.
+        s = str(num)
+        return int(s.replace('6', '9', 1))
