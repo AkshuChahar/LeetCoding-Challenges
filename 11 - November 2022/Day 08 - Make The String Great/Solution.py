3ca530c23f66412a63372d6cbd91b2bee70e29eb
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-11-08 23:38:19 +0530

    Add files via upload

---
+++ 11 - November 2022/Day 08 - Make The String Great/Solution.py
@@ -0,0 +1,12 @@
+﻿class Solution:
+    def makeGood(self, s: str) -> str:
+        # Initialise a stack.
+        st = []
+        # Traverse the string.
+        for ch in s:
+            # Check if the given condition is satisfied.
+            if st and st[-1] == ch.swapcase():
+                st.pop()
+            else:
+                st.append(ch)
+        return "".join(st)
