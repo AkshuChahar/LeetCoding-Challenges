1a8cc28876f802516e6e649c7e93d1feb07cf560
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-11-09 23:16:57 +0530

    Add files via upload

---
+++ 11 - November 2022/Day 09 - Online Stock Span/Solution.py
@@ -0,0 +1,19 @@
+﻿class StockSpanner:
+
+    def __init__(self):
+        # Initialise a stack to store the spans.
+        self.st = []
+
+    def next(self, price: int) -> int:
+        ans = 1
+        # Traverse the stack.
+        while self.st and self.st[-1][0] <= price:
+            # Update the stack and 'ans'.
+            ans += self.st.pop()[1]
+        self.st.append([price, ans])
+        return ans
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
