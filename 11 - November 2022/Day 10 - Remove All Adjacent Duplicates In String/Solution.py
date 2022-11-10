1f64c09ff82d03a4095d752f90a8ceb61cfc2aba
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-11-11 04:33:58 +0530

    Add files via upload

---
+++ 11 - November 2022/Day 10 - Remove All Adjacent Duplicates In String/Solution.py
@@ -0,0 +1,12 @@
+﻿class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        # Initialise a stack.
+        st = []
+        # Traverse the string.
+        for ch in s:
+            # Check if we same adjacent characters.
+            if st and st[-1] == ch:
+                st.pop()
+            else:
+                st.append(ch)
+        return "".join(st)
