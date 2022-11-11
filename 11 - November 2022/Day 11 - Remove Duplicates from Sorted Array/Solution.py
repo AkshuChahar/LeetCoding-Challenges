6b75aae893fc98b08a1fa99c0ca05d466b181103
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-11-11 19:17:08 +0530

    Add files via upload

---
+++ 11 - November 2022/Day 11 - Remove Duplicates from Sorted Array/Solution.py
@@ -0,0 +1,11 @@
+﻿class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n, ind = len(nums), 1
+        # Traverse the list.
+        for i in range(1, n):
+            # Find the unique element.
+            if nums[i - 1] != nums[i]:      
+                nums[ind] = nums[i] 
+                # Update 'ind'.
+                ind += 1       
+        return ind
