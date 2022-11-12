fae62c242631761b99a05d2b1dc8a84b662ffca3
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-11-12 16:45:37 +0530

    Add files via upload

---
+++ 11 - November 2022/Day 12 - Find Median from Data Stream/Solution.py
@@ -0,0 +1,23 @@
+﻿# Import 'SortedList'.
+from sortedcontainers import SortedList
+
+class MedianFinder:
+
+    def __init__(self):
+        # Store the data in a sorted list.
+        self.nums = SortedList()
+
+    def addNum(self, num: int) -> None:
+        # Add 'num' to the list.
+        self.nums.add(num)
+
+    def findMedian(self) -> float:
+        # Return the middle element.
+        n = len(self.nums)
+        return (self.nums[n//2] + self.nums[(n - 1)//2]) / 2
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
