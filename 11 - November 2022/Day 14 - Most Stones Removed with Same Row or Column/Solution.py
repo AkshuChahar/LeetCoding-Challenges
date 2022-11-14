1513d98d9c313a43159a52f41bcf5f16ac92201e
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-11-14 23:14:44 +0530

    Add files via upload

---
+++ 11 - November 2022/Day 14 - Most Stones Removed with Same Row or Column/Solution.py
@@ -0,0 +1,28 @@
+﻿class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        n, res = len(stones), 0
+        # Initialise dictionaries to store the connected components and a set to keep track of visited stones.
+        row_dict, col_dict = defaultdict(list), defaultdict(list)
+        vis = set()
+        for stone in stones:
+            row_dict[stone[0]].append(stone[1])
+            col_dict[stone[1]].append(stone[0])
+        # Function for 'DFS'.
+        def dfs(stone):
+            # Check if the current 'stone' has been visited.
+            if stone in vis:
+                return
+            # Update 'vis'.
+            vis.add(stone)
+            row, col = stone
+            # Recurse.
+            for c in row_dict[row]:
+                dfs((row, c))
+            for r in col_dict[col]:
+                dfs((r, col))
+        # Do a 'DFS'.
+        for r, c in stones:
+            if (r, c) not in vis:
+                dfs((r, c))
+                res += 1
+        return n - res
