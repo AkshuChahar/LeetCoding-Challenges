1431b60be168028047a9f544b9c4832af5fb19f4
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-11-23 23:18:44 +0530

    Add files via upload

---
+++ 11 - November 2022/Day 23 - Valid Sudoku/Solution.py
@@ -0,0 +1,11 @@
+﻿class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # Initialise a list to store the visited cells.
+        vis = []
+        # Traverse the board.
+        for i, row in enumerate(board):
+            for j, ele in enumerate(row):
+                # Check if we have found a digit.
+                if ele != ".":
+                    vis.extend([(ele, i), (j, ele), (ele, i//3, j//3)])
+        return len(set(vis)) == len(vis)
