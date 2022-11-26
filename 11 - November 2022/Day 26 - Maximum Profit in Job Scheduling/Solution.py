3ebdcd27610db314228494aa61e34ac1a98ae86a
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-11-26 10:08:14 +0530

    Add files via upload

---
+++ 11 - November 2022/Day 26 - Maximum Profit in Job Scheduling/Solution.py
@@ -0,0 +1,12 @@
+﻿class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        # Initialise a list of tuples to get all the jobs sorted according to the start time.
+        jobs, n = sorted(zip(startTime, endTime, profit)), len(startTime)
+        # Initialise a dp list.
+        dp = [0] * (n + 1)
+        # Traverse the list.
+        for i in reversed(range(n)):
+            # Perform a 'Binary Search'.
+            ind = bisect_left(jobs, jobs[i][1], key = lambda j: j[0])
+            dp[i] = max(jobs[i][2] + dp[ind], dp[i+1])
+        return dp[0]
