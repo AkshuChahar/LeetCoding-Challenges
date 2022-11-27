0b6e724a7b7318676a731d6222cf52c95f54c8d7
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-11-27 23:39:46 +0530

    Add files via upload

---
+++ 11 - November 2022/Day 27 - Arithmetic Slices II - Subsequence/Solution.py
@@ -0,0 +1,13 @@
+﻿class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        ans, n = 0, len(nums)
+        # Initialise a list of dictionaries to store the possible combinations.
+        dp = [defaultdict(int) for _ in nums]
+        # Traverse the list.
+        for i in range(1, n):
+            for j in range(i):
+                # Calculate the difference and update the 'dp'.
+                diff = nums[j] - nums[i]
+                dp[i][diff] += dp[j][diff] + 1
+                ans += dp[j][diff]
+        return ans
