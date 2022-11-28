a8f85a2a1ac1f3f65ab86f2bcc3955f112dc45d0
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-11-28 20:37:50 +0530

    Add files via upload

---
+++ 11 - November 2022/Day 28 - Find Players with Zero or One Losses/Solution.py
@@ -0,0 +1,22 @@
+﻿class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        ans = [[], []]
+        # Initialise a list to keep track of the wins and losses of each player.
+        cnt = [-1] * 100001
+        # Traverse the list.
+        for winner, loser in matches:
+            # Check if the player has not played any match and won the match.
+            if cnt[winner] == -1:
+                cnt[winner] = 0
+            # Check if the player has not played any match and lost the match.
+            if cnt[loser] == -1:
+                cnt[loser] = 1
+            else:
+                cnt[loser] += 1
+        # Traverse the list of scores and update 'ans'.
+        for i in range(100001):
+            if cnt[i] == 0:
+                ans[0].append(i)
+            elif cnt[i] == 1:
+                ans[1].append(i)   
+        return ans
