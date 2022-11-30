01281ce8f4abdb9cf41ddc773bb32e4ef7ade8b8
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-11-30 19:38:17 +0530

    Add files via upload

---
+++ 11 - November 2022/Day 30 - Unique Number of Occurrences/Solution.py
@@ -0,0 +1,4 @@
+﻿class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        # Check for number of elements in the list and the number of elements in the list of count of elements of list.
+        return len(set(Counter(arr).values())) == len(set(arr))
