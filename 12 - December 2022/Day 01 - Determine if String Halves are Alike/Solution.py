832b3b56e977b6bb8e781482ca5e8aeeee1ab023
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-12-01 19:41:08 +0530

    Add files via upload

---
+++ 12 - December 2022/Day 01 - Determine if String Halves are Alike/Solution.py
@@ -0,0 +1,14 @@
+﻿class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        sum_a = sum_b = 0
+        # Initialise a set of vowels.
+        vowels = set('aeiouAEIOU')
+        # Traverse the word.
+        for i in range(len(s)//2):
+            # Check for first half.
+            if s[i] in vowels:
+                sum_a += 1
+            # Check for second half.
+            if s[len(s)//2 + i] in vowels:
+                sum_b += 1
+        return sum_a == sum_b
