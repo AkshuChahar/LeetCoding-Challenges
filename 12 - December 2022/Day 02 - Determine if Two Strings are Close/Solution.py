bf90b64618caabd7440a1ad11b227f54124f53e9
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-12-02 21:40:26 +0530

    Add files via upload

---
+++ 12 - December 2022/Day 02 - Determine if Two Strings are Close/Solution.py
@@ -0,0 +1,4 @@
+﻿class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        # Count the frequency and number of unique characters.
+        return Counter(Counter(word1).values()) == Counter(Counter(word2).values()) and set(word1) == set(word2)
