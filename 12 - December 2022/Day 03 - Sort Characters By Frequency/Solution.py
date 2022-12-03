caacad86f674ad5c67484789f5f4577159d6c805
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-12-03 22:26:05 +0530

    Add files via upload

---
+++ 12 - December 2022/Day 03 - Sort Characters By Frequency/Solution.py
@@ -0,0 +1,4 @@
+﻿class Solution:
+    def frequencySort(self, s: str) -> str:
+        # Count the frequency of each character and then join to form the string.
+        return "".join(sorted(s, key = lambda ch, freq = Counter(s): (-freq[ch], ch)))
