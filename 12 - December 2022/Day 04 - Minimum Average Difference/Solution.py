607e07b755a6ca5ac11812248245fb0d0e1b038b
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-12-04 09:02:16 +0530

    Add files via upload

---
+++ 12 - December 2022/Day 04 - Minimum Average Difference/Solution.py
@@ -0,0 +1,21 @@
+﻿class Solution:
+    def minimumAverageDifference(self, nums: List[int]) -> int:
+        n, ans, diff, pref_sum = len(nums), -1, math.inf, 0
+        # Calculate total sum of list.
+        total = sum(nums)
+        # Traverse the list.
+        for ind in range(n):
+            pref_sum += nums[ind]
+            # Calculate average of left and right part of list.
+            left = pref_sum
+            left //= (ind + 1)
+            right = total - pref_sum
+            # Check if 'right' part have 0 elements.
+            if ind != n - 1:
+                right //= (n - ind - 1)
+            curr_diff = abs(left - right)
+            # Update 'diff' and 'ans'.
+            if curr_diff < diff:
+                diff = curr_diff
+                ans = ind                
+        return ans
