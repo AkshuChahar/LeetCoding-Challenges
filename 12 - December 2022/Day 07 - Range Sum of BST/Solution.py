e1b09f23052bb31a70a38cab07c69fc68d876648
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-12-07 08:11:42 +0530

    Add files via upload

---
+++ 12 - December 2022/Day 07 - Range Sum of BST/Solution.py
@@ -0,0 +1,32 @@
+﻿# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        ans = 0
+        # Initialise queue for 'Breadth-First Search'.
+        q = deque()
+        q.append(root)
+        # Perform 'Breadth-First Search'.
+        while q:
+            n = len(q)
+            while n:
+                curr = q.popleft()
+                # Update 'ans'.
+                if low <= curr.val <= high:
+                    ans += curr.val
+                    if curr.left:
+                        q.append(curr.left)
+                    if curr.right:
+                        q.append(curr.right)
+                # Check for next node.
+                elif low > curr.val and curr.right:
+                    q.append(curr.right)
+                elif high < curr.val and curr.left:
+                    q.append(curr.left)
+                n -= 1
+        return ans
