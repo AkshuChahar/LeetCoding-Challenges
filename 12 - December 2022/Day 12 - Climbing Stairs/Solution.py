b853e48d113007a0ff2d802bc4997ce6f18f2b8f
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-12-13 10:34:00 +0530

    Add files via upload

---
+++ 12 - December 2022/Day 12 - Climbing Stairs/Solution.py
@@ -0,0 +1,11 @@
+﻿class Solution:
+    def climbStairs(self, n: int) -> int:
+        # Edge case: Check if the number is less than 1.
+        if n < 1:
+            return 1
+        # Initialise the 'dp' list.
+        dp = [1] * 2
+        # Iterate for all stairs.
+        for i in range(2, n+1):
+            dp[1], dp[0] = dp[1] + dp[0], dp[1]
+        return dp[1]
