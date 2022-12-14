7dc224c225f5bc68bd15594591647aaaa7270a27
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-12-14 23:15:35 +0530

    Add files via upload

---
+++ 12 - December 2022/Day 14 - House Robber/Solution.py
@@ -0,0 +1,13 @@
+﻿class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        # Edge case: Check if there is only 1 house.
+        if (n == 1):
+            return nums[0]
+        # Initialise a 'dp' list.
+        dp = [0]
+        dp.append(nums[0])
+        # Traverse the list.
+        for i in range(2, n + 1):
+            dp.append(max(dp[i - 1], nums[i - 1] + dp[i - 2]))
+        return dp[n]
