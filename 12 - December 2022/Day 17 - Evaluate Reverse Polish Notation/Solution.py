d741552a30af5f8b8286bddfde89f41e9a957055
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-12-17 07:40:42 +0530

    Add files via upload

---
+++ 12 - December 2022/Day 17 - Evaluate Reverse Polish Notation/Solution.py
@@ -0,0 +1,22 @@
+﻿class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        # Initialise a stack.
+        s = []
+        # Traverse the list.
+        for token in tokens:
+            # Check if we have found a digit or operator.
+            if token not in "+-*/":
+                s.append(int(token))
+            else:
+                # Get two digits.
+                right, left = s.pop(), s.pop()
+                # Perform the operation.
+                if token == "+":
+                    s.append(left + right)
+                elif token == "-":
+                    s.append(left - right)
+                elif token == "*":
+                    s.append(left * right)
+                else:
+                    s.append(int(float(left) / right))
+        return s.pop()
