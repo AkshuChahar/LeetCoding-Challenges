0bdbdc45492fb9dea6f307e94ec4be25d58325e1
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-12-18 19:54:24 +0530

    Add files via upload

---
+++ 12 - December 2022/Day 18 - Daily Temperatures/Solution.py
@@ -0,0 +1,14 @@
+﻿class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        ans = [0] * n
+        # Initialise a stack.
+        s = []
+        # Traverse the list.
+        for i, temp in enumerate(temperatures):
+            # Check for warmer temperatures.
+            while s and temperatures[s[-1]] < temp:
+                curr = s.pop()
+                ans[curr] = i - curr
+            s.append(i)
+        return ans
