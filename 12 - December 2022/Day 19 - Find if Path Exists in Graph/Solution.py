90e0a0d71054ed2e6907e6e6ffe2e4fb82d63ae9
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-12-20 00:28:58 +0530

    Add files via upload

---
+++ 12 - December 2022/Day 19 - Find if Path Exists in Graph/Solution.py
@@ -0,0 +1,22 @@
+﻿class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        # Initialise a list to keep track of visited nodes and a stack.
+        seen = [False] * n
+        seen[source] = True
+        s = [source]
+        # Store all edges according to nodes in 'graph'.
+        graph = collections.defaultdict(list)
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+        # Traverse the graph.
+        while s:
+            curr_node = s.pop()
+            # Add all unvisited neighbors of the current node to stack and mark them as visited.
+            for next_node in graph[curr_node]:
+                if next_node == destination:
+                    return True
+                if not seen[next_node]:
+                    seen[next_node] = True
+                    s.append(next_node)   
+        return seen[destination]
