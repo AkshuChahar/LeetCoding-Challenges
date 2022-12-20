c66130ebda96000b8c1ed297387e4280d8922147
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-12-20 22:00:11 +0530

    Add files via upload

---
+++ 12 - December 2022/Day 20 - Keys and Rooms/Solution.py
@@ -0,0 +1,20 @@
+﻿class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        n = len(rooms)
+        # Initialise a stack and a list to keep track of visited rooms.
+        s, visited = [], [False] * n
+        s.append(0)
+        visited[0] = True
+        # Traverse the rooms.
+        while s:
+            room = s.pop()
+            for r in rooms[room]:
+                # Check if we have already visited the room.
+                if not visited[r]:
+                    s.append(r)
+                    visited[r] = True
+        # Check if we have visited all the rooms.
+        for room in visited:
+            if not room:
+                return False
+        return True
