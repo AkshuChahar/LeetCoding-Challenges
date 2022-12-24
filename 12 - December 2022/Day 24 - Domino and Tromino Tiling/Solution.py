1b190b09b9003d3c88e26eb39b3b4991c8866251
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-12-24 22:10:14 +0530

    Add files via upload

---
+++ 12 - December 2022/Day 24 - Domino and Tromino Tiling/Solution.py
@@ -0,0 +1,9 @@
+﻿class Solution:
+    def numTilings(self, n: int) -> int:
+        # Initialise the 'dp' list.
+        dp = [1, 2, 5] + [0] * n
+        # Traverse for all blocks.
+        for i in range(3, n):
+            # Update 'dp'.
+            dp[i] = (dp[i - 1] * 2 + dp[i - 3]) % 1000000007
+        return dp[n - 1]
