df6614b54b1396d9b21678acbaab83378310d218
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-12-25 19:50:59 +0530

    Add files via upload

---
+++ 12 - December 2022/Day 25 - Longest Subsequence With Limited Sum/Solution.py
@@ -0,0 +1,13 @@
+﻿class Solution:
+    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        ans, n = [], len(nums)
+        # Calculate the prefix sum list of the sorted 'nums'.
+        nums.sort()
+        for i in range(1, n):
+            nums[i] += nums[i - 1]
+        # Traverse the list.
+        for query in queries:
+            # Find the insertion index of the prefix sum list.
+            ind = bisect.bisect_right(nums, query)
+            ans.append(ind)
+        return ans
