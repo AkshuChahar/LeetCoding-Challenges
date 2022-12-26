e3abc11796b0529b55815be97e608887e58e6b61
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-12-26 21:38:07 +0530

    Add files via upload

---
+++ 12 - December 2022/Day 26 - Jump Game/Solution.py
@@ -0,0 +1,11 @@
+﻿class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        last, n = 0, len(nums)
+        # Traverse the list.
+        for i in range(n):
+            # Check if the current step can be reached.
+            if (last < i):
+                break
+            # Update 'last'.
+            last = max(last, nums[i]+i)
+        return last >= n - 1
