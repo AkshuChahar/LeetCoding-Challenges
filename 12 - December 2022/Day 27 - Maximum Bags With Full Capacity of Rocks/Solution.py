761c1c7f7406b8f1f093dd81f13806d38f608eaa
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-12-27 21:20:27 +0530

    Add files via upload

---
+++ 12 - December 2022/Day 27 - Maximum Bags With Full Capacity of Rocks/Solution.py
@@ -0,0 +1,15 @@
+﻿class Solution:
+    def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
+        ans = 0
+        # Calculate the emptiness of each bag.
+        empty = [total - curr for total, curr in zip(capacity, rocks)]
+        empty.sort()
+        # Traverse the sorted list.
+        for cap in empty:
+            # Check if current bag can be filled.
+            if cap <= additionalRocks:
+                additionalRocks -= cap
+                ans += 1
+            else:
+                break
+        return ans
