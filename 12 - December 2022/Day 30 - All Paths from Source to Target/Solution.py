e662da9484f34ee7852ec7bd9b619303422815d8
Akshu Chahar <50517094+AkshuChahar@users.noreply.github.com>
2022-12-31 18:45:44 +0530

    Add files via upload

---
+++ 12 - December 2022/Day 30 - All Paths from Source to Target/Solution.py
@@ -0,0 +1,18 @@
+﻿class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        n = len(graph)
+        ans = []
+        # Initialise a queue to traverse the graph.
+        q = deque()
+        for child in graph[0]:
+            q.append((child, [0]))
+        # Traverse the graph.
+        while q:
+            curr, path = q.popleft()
+            # Check if we have reached the end of the path.
+            if curr == n - 1:
+                ans.append(path + [curr])
+            # Update queue.
+            for child in graph[curr]:
+                q.append((child, path + [curr]))
+        return ans
